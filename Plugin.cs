5e0a3349bda2f7eb226cab34deb2fc273be0b4ff
Jari Suutari <jari.suutari@gmail.com>
2024-12-19 19:25:53 +0200

    Converted to plugin

---
+++ Plugin.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using RIoT2.Core.Interfaces;
+using RIoT2.Core.Interfaces.Services;
+using RIoT2.Net.Devices.Catalog;
+
+namespace RIoT2.Net.Devices
+{
+    public class Plugin : IDevicePlugin
+    {
+        private List<IDevice> _devices;
+
+        public Plugin(
+            INodeConfigurationService configurationService,
+            ILogger<Plugin> logger,
+            IWebhookService webhookService,
+            IFtpService ftpService,
+            IStorageService storageService,
+            IDownloadService downloadService,
+            IMemoryStorageService memoryStorageService,
+            IAzureRelayService azureRelayService) {
+            _devices = [
+                //new Web(logger, webhookService),
+                //new Catalog.Timer(logger),
+                //new Virtual(logger),
+                //new Mqtt(logger),
+                new WaterConsumption(logger),
+                new Messaging(logger),
+                new FTP(logger, ftpService, downloadService, memoryStorageService),
+                new ElectricityPrice(logger),
+                new EasyPLC(logger),
+                //new NetatmoWeather(logger),
+                new NetatmoSecurity(logger, downloadService, memoryStorageService),
+                new Hue(logger),
+                new AzureRelay(logger, azureRelayService)
+            ];
+        }
+
+        public List<IDevice> Devices 
+        {
+            get { return _devices; }
+        }
+    }
+}
