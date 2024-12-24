c0644fe26464639367b12171032f8ecdace8303e
Jari Suutari <jari.suutari@gmail.com>
2024-12-24 19:29:18 +0200

    added IServiceProvider

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using RIoT2.Core.Interfaces;
 using RIoT2.Core.Interfaces.Services;
 using RIoT2.Net.Devices.Catalog;
@@ -9,21 +10,23 @@ namespace RIoT2.Net.Devices
     {
         private List<IDevice> _devices;
 
-        public Plugin(
-            INodeConfigurationService configurationService,
-            ILogger<Plugin> logger,
-            IWebhookService webhookService,
-            IFtpService ftpService,
-            IStorageService storageService,
-            IDownloadService downloadService,
-            IMemoryStorageService memoryStorageService,
-            IAzureRelayService azureRelayService) {
+        public Plugin(IServiceProvider services)
+        {
+            INodeConfigurationService configurationService = services.GetRequiredService<INodeConfigurationService>();
+            ILogger logger = services.GetRequiredService<ILogger>();
+            IWebhookService webhookService = services.GetRequiredService<IWebhookService>();
+            IFtpService ftpService = services.GetRequiredService<IFtpService>();
+            IStorageService storageService = services.GetRequiredService<IStorageService>();
+            IDownloadService downloadService = services.GetRequiredService<IDownloadService>();
+            IMemoryStorageService memoryStorageService = services.GetRequiredService<IMemoryStorageService>();
+            IAzureRelayService azureRelayService = services.GetRequiredService<IAzureRelayService>();
+
             _devices = [
-                //new Web(logger, webhookService),
-                //new Catalog.Timer(logger),
-                //new Virtual(logger),
-                //new Mqtt(logger),
-                new WaterConsumption(logger),
+               //new Web(logger, webhookService),
+               //new Catalog.Timer(logger),
+               //new Virtual(logger),
+               //new Mqtt(logger),
+               new WaterConsumption(logger),
                 new Messaging(logger),
                 new FTP(logger, ftpService, downloadService, memoryStorageService),
                 new ElectricityPrice(logger),
@@ -32,10 +35,10 @@ namespace RIoT2.Net.Devices
                 new NetatmoSecurity(logger, downloadService, memoryStorageService),
                 new Hue(logger),
                 new AzureRelay(logger, azureRelayService)
-            ];
+           ];
         }
 
-        public List<IDevice> Devices 
+        public List<IDevice> Devices
         {
             get { return _devices; }
         }
